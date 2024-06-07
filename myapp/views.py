3cc15bd011420fbe6ffc4066d2b8d317ceaf64f7
muslimbek77 <muslimjonbekali@mail.ru>
2024-06-07 10:05:32 +0500

    connect template

--- myapp/views.py
+++ myapp/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-
-
-
 def hello_world(request):
-    return HttpResponse("<h1>Assalomu alaykum Sifat</h1>")
+    return render(request=request,template_name="index.html")
+
+def hi_world(request):
+    return render(request=request,template_name="hi.html")
